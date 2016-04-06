996110b13eb75b0fb60c9d04b807c903c753b487
ildargilfanov <ildar.gilfanov@csssr.com>
2016-04-06 07:58:03 +0300

    feat(bg) new urls

--- background.js
+++ background.js
@@ -1,10 +1,44 @@
+var that = this;
+
+var domens = [
+        {
+            url: [
+                'https://www.youtube.com/',
+                'https://www.youtube.com/?gl=RU&hl=ru',
+                'https://www.youtube.com/?gl=RU&=ru'
+            ],
+            rederect: 'https://www.youtube.com/feed/subscriptions'
+        },
+        {
+            url: [
+                'https://vk.com/feed',
+                'https://new.vk.com/feed'
+            ],
+            rederect: 'https://vk.com/id0',
+        }
+    ];
+
 chrome.tabs.onUpdated.addListener(function(data) {
-	chrome.tabs.getSelected(null, function(tab) {
- 		if(tab.url === 'https://www.youtube.com/') {
- 			chrome.tabs.update(tab.id, {url: 'https://www.youtube.com/feed/subscriptions'});
- 		};
- 		if(tab.url === 'https://vk.com/feed') {
- 			chrome.tabs.update(tab.id, {url: 'https://vk.com/id0'});
- 		};
+    chrome.tabs.getSelected(null, function(tab) {
+
+        var current = that.find(tab);
+
+        if(current){
+            chrome.tabs.update(tab.id, {url: current.rederect});
+        }
+
     });
 });
+
+function find(tab) {
+    return _.find(domens, function(obj) {
+        if(index(obj, tab.url) > -1){
+            return obj;
+        }
+    });
+}
+function index(obj, url){
+    return _.findIndex(obj.url, function(s) {
+        return s == url;
+    })
+}
